adfc2ce5bd021931a155036c39b88604e03ed7be
jrunge <jrunge@precisionplanting.com>
2023-11-19 22:56:09 -0600

    String passed

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 
-#[no_mangle]
-pub extern "C" fn greet(name: *const c_char) -> *const u8 {
-    let c_str = unsafe { CStr::from_ptr(name) };
-    let str_slice: &str = c_str.to_str().unwrap();
-    let s: String = str_slice.to_owned();  // Use str_buf as needed
-    println!("{}", s);
+const SOME_MEMORY_ALLOCATION: &str = "Will this show up?";
+const INTS_IN_MEMORY: i32 = 100;
 
-    s.as_ptr()
+#[no_mangle]
+pub fn greet() -> String {
+    println!("{}, {}", SOME_MEMORY_ALLOCATION, INTS_IN_MEMORY);
+    let _int = INTS_IN_MEMORY.clone();
+    let _str = SOME_MEMORY_ALLOCATION.clone();
+    let s = format!("Hello!");
+    s
 }
 
 #[no_mangle]
@@ -19,6 +21,19 @@ pub extern "C" fn greet_len(name: *const c_char) -> u8 {
     let len = s.len() as u8;
 
     len
+}
+
+static HELLO: &'static str = "hello from rust";
+
+#[no_mangle]
+pub extern "C" fn get_hello() -> *mut c_char {
+    let s = CString::new(HELLO).unwrap();
+    s.into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn get_hello_len() -> usize {
+    HELLO.len()
 }
 
 // #[cfg(test)]
